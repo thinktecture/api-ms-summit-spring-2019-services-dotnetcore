8b28b7484d97f39a57ec800e0cf4a7ff004a675f
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2018-12-04 09:29:05 +0100

    Added docker startup scripts for windows

---
+++ services/mssql-windows.bat
@@ -0,0 +1,1 @@
+docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=th1sIsStr()ng" -p 1433:1433 --name sqlapisummit -d mcr.microsoft.com/mssql/server:2017-latest
---
+++ services/seq-docker-windows.bat
@@ -0,0 +1,1 @@
+docker run -e "ACCEPT_EULA=Y" -p 5341:80 --name seqapisummit -d datalust/seq:latest
