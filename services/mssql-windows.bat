55ed2be6a64e2775cd48ce3f33ed92cac7c490e1
Manuel Rauber <manuel.rauber@thinktecture.com>
2019-06-18 09:31:38 +0200

    Use the current SQL Server Docker Images

--- services/mssql-windows.bat
+++ services/mssql-windows.bat
@@ -1,1 +1,1 @@
-docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=th1sIsStr()ng" -p 1433:1433 --name sqlapisummit -d mcr.microsoft.com/mssql/server:2017-latest
+docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=th1sIsStr()ng" -e "MSSQL_PID=Express" -p 1433:1433 -v sqlvolume:/var/opt/mssql microsoft/mssql-server-linux:latest
